f196f9fdd7e5f57d59eeb5faa515c0c02a188499
ajayygurjar <ajay1651@gmail.com>
2025-01-10 15:22:26 +0530

    Summary of the restaurant app

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MealsSummary from './MealsSummary';
+
+
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
