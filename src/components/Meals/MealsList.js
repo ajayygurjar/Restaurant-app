725f48006c3f790ba8870b8e6869ac6216e74af8
ajayygurjar <ajay1651@gmail.com>
2025-01-12 11:55:07 +0530

    Updated UI and separate component for list

--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -1,39 +1,52 @@
-import classes from './MealsList.module.css'
-import MealForm from './MealForm';
+import classes from "./MealsList.module.css";
+
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
-const MealsList=()=>{
+const MealsList = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
-    return <div className={classes.container}><ul>
-        {DUMMY_MEALS.map(meal=><li className={classes.item} key={meal.id}>{meal.name}: {meal.description} (${meal.price}) <MealForm/>  </li>)}
-    </ul>
+  return (
+    <div className={classes.container}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </div>
-}
+  );
+};
 
-export default MealsList
+export default MealsList;
