8c198247ec1ec19d6926028a01e892c0b14ee0e0
ajayygurjar <ajay1651@gmail.com>
2025-01-12 17:37:31 +0530

    Cart open and close function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { Fragment } from 'react';
+import { Fragment,useState } from 'react';
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const[cartState,setCartState] =useState(false)
+
+  const onCartClick=()=>{
+    setCartState(prevState=>!prevState);
+  }
+  
   return (
     <Fragment>
-      <Cart/>
-      <Header/>
+      {cartState && <Cart onCartClick={onCartClick}/>}
+      <Header onCartClick={onCartClick}/>
       <main><Meals/></main> {/* meals component should be used outside of header or  after header */}
     </Fragment>
   );
